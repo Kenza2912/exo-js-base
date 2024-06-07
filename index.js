a615497a997a19e8d448aeaf01a5c1bd91145ac2
Kenza <a.kenza29@gmail.com>
2024-06-07 16:56:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+     
+
+// window.addEventListener("load", (event) => {
+
+
+    // function afficherInfo(element){
+    //     element.style.width,
+    //     element.style.width,
+    //     element.style.width,
+    //     element.style.width,
+    //     element.style.width
+    //     }
+        //faire une foncitoon ou vous recuperez toute les info 
+        //width height color
+    //     let recupe
+    //     rer = document.getElementById("carre")
+    //      car = getComputedStyle(recuperer,null).color
+    // console.log(car)
+//    console.log(recuperer.style.height)
+
+// console.log(afficherInfo(recuperer))
+//   });
+
+
+
+let recuperer = document.getElementById("carre")
+color = getComputedStyle(recuperer, null).color
+
+alert(color)
