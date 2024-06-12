4941491dcae0454d001dcbac83b0a63e45f952de
Kenza <a.kenza29@gmail.com>
2024-06-12 13:28:05 +0200

    suite exo

--- exo5/index.js
+++ exo5/index.js
@@ -3,6 +3,29 @@
 const message = document.querySelector(".message")
 console.log(message)
 let conv = 6.55957
+const input = document.getElementById("valeur")
+console.log(input)
+const result = document.querySelector(".result")
+console.log(result)
+
+
+
+
+
+
+input.addEventListener("input",function(){
+    const changeNb = input.value
+    if(Number.isNaN(changeNb)){
+        
+        message.textContent = "Vous devez saisir une valeur numérique"
+    }else{
+        result.textContent = (parseFloat(changeNb)*conv).toFixed(2)
+     
+        
+    }
+})
+
+
 
 
 
